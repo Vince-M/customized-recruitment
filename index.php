c30c3cc0cd96ed4b3f5872f081cc0d044a94f1fb
Vince Mendella <vmendella@gmail.com>
2023-03-04 17:15:13 -0600

    modify type css, make index.php into blog listing page

--- index.php
+++ index.php
@@ -2,118 +2,42 @@
 
     <!-- HERO
     =================================================== -->
-    <section class="hero feature-image frontPage__hero" role="banner" data-type="background" data-speed="2">
+    <section class="header__hero " role="banner" data-type="background" data-speed="2">
+      <div class="header__hero--image">
+        <img src="<?php the_field( 'hero_images' ); ?>" alt="">
+      </div>
 
-      <div class="frontPage__hero--headline">
-        <h1 class="hero__header--fullPage">Recruiting top talent is an art, a science, and a professional calling.</h1>
-        <h2 class="hero__header--fullPage">Attracting the right candidates for a position is the first step in making a successful hire.</h2>
-        <h2 class="hero__header--fullPage">Expertly managing the talent acquisition process, reducing time from attracting and hiring.</h2>
-        <div class="btn btn__hero btn__contact">
-          <a href="#">Contact Us</a>
-        </div> <!-- btn btn__contact -->
-      </div> <!-- hero__headline -->
-
+      <div class="header__hero--content">
+        <h1>Welcome to our Blog!</h1>
+      </div>
+      
     </section> <!-- hero feature-image -->
     <!-- ============================================== -->
 
-    <!-- ABOUT CUSTOMISZED RECRUITMENT
-    =================================================== -->
-      <section class="full__width full__width--about">
-        <div class="container frontpg__about">
-          <div class="frontpg__about--img">
-            <img src="img/frontpg-about_600x326-min.jpg" alt="about Customized Recruitment" width="600">
-          </div>
-          <div class="frontpg__about--text">
-            <h2>About Customized Recruitment</h2>
-            <p>A vision for a different recruiter–client relationship to drive value and expedite results.</p>
-            <div class="btn btn__about">
-              <a href="about.html">Learn More</a>
-            </div> <!-- btn btn__contact -->
-          </div>
+      <!-- CONTENT
+  =================================================== -->
+
+  <section class="container">
+    <div class="content">
+      
+    <?php 
+      while( have_posts() ) {
+        the_post(); ?>
+
+        <div class="post-item">
+          <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
         </div>
-      </section>
-    <!-- ============================================== -->
-
-    <!-- RECRUITING SERVICES
-    =================================================== -->
-      <section class="full__width full__width--recruiting">
-        <div class="container frontpg__recruiting">
-          <div class="frontpg__recruiting--img">
-            <img src="img/frontpg_recruiting_670x550.jpg" alt="Recruiting Services" width="670">
-          </div>
-          <div class="frontpg__recruiting--text">
-            <h2>Recruiting Services</h2>
-            <p>Dedicated recruiting support without the added headcount.</p>
-            <div class="btn btn__recruiting">
-              <a href="recruiting.html">Learn More</a>
-            </div>
-          </div>
-
+        <div class="meta-box">
+          <p>Posted on February 20, 2023</p>
         </div>
-      </section>
-    <!-- ============================================== -->
-
-    <!-- LET'S CONNECT
-    =================================================== -->
-      <section class="full__width full__width--connect">
-        <div class="container page__connect">
-          <h2>Let's Connect</h2>
-          <div class="page__connect--input">
-            <input type="text" class="index__input" id="Name" name="Name" placeholder="Full Name" size="40">
-            <input type="email" class="index__input" id="Email" name="Email" placeholder="Email" size="40">
-          </div> <!-- page__connect--input -->
-        </div> <!-- container page__connect -->
-      </section> <!-- full__width full__width--connect -->
-    <!-- ============================================== -->
 
 
-    <!-- CLIENT TESTIMONIALS
-    =================================================== -->
-      <section class="frontPage__testimonials">
-        
-          <div class="frontpg__testimonials frontPage__wrapper">
-            <div class="frontPage__wrapper--image">
-            </div> <!-- frontPage__wrapper--image -->
+    <?php  }
+    ?>
+    </div> 
+  </section> 
 
-            <div class="frontPage__wrapper--text">
-              <h3>Client & Candidate Testimonials...</h3>
-            </div> <!-- frontPage__wrapper--text -->
-          </div> <!-- container frontpg__testimonials -->
-        
+  <!-- ============================================== -->
 
-        <div class="container frontPage__testimonials--section">
-          <div class="frontPage__testimonials--testimonial fadeIn">
-            <div class="testimonial__logo">
-              <img src="img/ha_logo_dark_400x63.png" alt="Harbour Air Seaplanes logo" width="400px">
-            </div> <!-- testimonial__logo -->
-            <div class="testimonial__excerpt">
-              <p>
-                I absolutely trust the process that Customized Recruitment uses to identify and onboard successful candidates across all areas of our business. They go above and beyond, guiding every step along the way...  
-              </p>
-              <div class="readMore">
-                <a href="testimonials-single.html">[ Read More ]</a>
-              </div>
-            </div> <!-- testimonial__excerpt -->
-          </div> <!-- frontPage__testimonials--testimonial -->
-  
-          <div class="frontPage__testimonials--testimonial fadeIn">
-            <div class="testimonial__logo">
-              <img src="img/denrayTire_210x70.jpg" alt="Denray Tire logo" width="210px">
-            </div> <!-- testimonial__logo -->
-            <div class="testimonial__excerpt">
-              <p>
-                While many candidates have the technical qualifications, Theresa ensures she is presenting candidates that not only meet the skills required for the role but are the best fit for our culture....              </p>
-              <div class="readMore">
-                <a href="testimonials-single-denray.html">[ Read More ]</a>
-              </div>
-            </div> <!-- testimonial__excerpt -->
-          </div> <!-- frontPage__testimonials--testimonial -->
-        </div> <!-- container frontPage__testimonials--section -->
-       
-        <div class="container testimonials__all">
-          <p><a href="testimonials.html">All Testimonials &raquo;</a></p>
-        </div> <!-- container testimonials__all -->
-      </section> <!-- frontPage__testimonials -->
-    <!-- ============================================== -->
 
 <?php get_footer(); ?>
